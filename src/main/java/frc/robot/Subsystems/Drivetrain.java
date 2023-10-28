073af3521c876e34726e2cf41d66ccb4123d6422
DriverStationComputer <driverstation@carlmontrobotics.org>
2023-10-27 18:40:42 -0700

    reorganize more so motors are 2 and turn method gone

--- src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -10,28 +10,12 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import org.carlmontrobotics.lib199.MotorControllerFactory;
 import org.carlmontrobotics.lib199.MotorConfig;
 public class Drivetrain {
-CANSparkMax motor1 = MotorControllerFactory.createSparkMax(1,MotorConfig.NEO_550);
-CANSparkMax motor2 = MotorControllerFactory.createSparkMax(2,MotorConfig.NEO_550);
-CANSparkMax motor3 = MotorControllerFactory.createSparkMax(3,MotorConfig.NEO_550);
-CANSparkMax motor4 = MotorControllerFactory.createSparkMax(4,MotorConfig.NEO_550);
-CANSparkMax motor5 = MotorControllerFactory.createSparkMax(5,MotorConfig.NEO_550);
-CANSparkMax motor6 = MotorControllerFactory.createSparkMax(6,MotorConfig.NEO_550);
-MotorControllerGroup leftMotors = new MotorControllerGroup(motor1,motor2,motor3);
-MotorControllerGroup rightMotors = new MotorControllerGroup(motor4, motor5, motor6);
-
+CANSparkMax leftMotors = MotorControllerFactory.createSparkMax(1,MotorConfig.NEO_550);
+CANSparkMax rightMotors = MotorControllerFactory.createSparkMax(2,MotorConfig.NEO_550);
 
 public void driveForward(double y1,double y2) {
     leftMotors.set(y1);
     rightMotors.set(y2);
-}
-public void turn(double x1) {
-    if(x1>0) {
-        leftMotors.set(x1);
-        rightMotors.set(1-x1);
-    } 
-    if(x1<0) {
-        leftMotors.set(1-x1);
-        rightMotors.set(x1);
-    }
+
 }
 }
