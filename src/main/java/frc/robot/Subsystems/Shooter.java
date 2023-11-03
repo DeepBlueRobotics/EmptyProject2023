2e3469a6b6bb35afbb904a6cbdfee183a38dac98
aaron345678 <94726964+aaron345678@users.noreply.github.com>
2023-11-03 23:13:00 +0000

    4 motors in shooter and fixed ports

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -11,8 +11,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Shooter extends SubsystemBase {
     CANSparkMax leftShooter1 = MotorControllerFactory.createSparkMax(7,MotorConfig.NEO_550);
     CANSparkMax rightShooter1 = MotorControllerFactory.createSparkMax(10,MotorConfig.NEO_550);
-    CANSparkMax leftShooter2 = MotorControllerFactory.createSparkMax(7,MotorConfig.NEO_550);
-    CANSparkMax rightShooter2 = MotorControllerFactory.createSparkMax(10,MotorConfig.NEO_550);
+    CANSparkMax leftShooter2 = MotorControllerFactory.createSparkMax(2,MotorConfig.NEO_550);
+    CANSparkMax rightShooter2 = MotorControllerFactory.createSparkMax(3,MotorConfig.NEO_550);
     MotorControllerGroup firstSet = new MotorControllerGroup(leftShooter1,rightShooter1);
     MotorControllerGroup secondSet = new MotorControllerGroup(leftShooter2,rightShooter2);
 
